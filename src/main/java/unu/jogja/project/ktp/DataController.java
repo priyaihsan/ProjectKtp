05e991a9eaea5ec36e77f51baf4f573f9ecb0add
priya ihsan <priyaihsan771@gmail.com>
2022-04-05 14:19:36 +0700

    first commit

---
+++ src/main/java/unu/jogja/project/ktp/DataController.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unu.jogja.project.ktp;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ *
+ * @author Ican
+ */
+@Controller
+public class DataController {
+    DataJpaController datactrl = new DataJpaController();
+    List<Data> newData = new ArrayList<>();
+    
+    @RequestMapping("/main")
+    public String getMain(){
+        return "menu";
+    }
+    
+    @RequestMapping("/data")
+    public String getDataKTP(Model model){
+        try{
+            newData = datactrl.findDataEntities();
+        }catch(Exception e){}
+        model.addAttribute("getdata",newData);
+        return "data";
+    }
+    
+    @RequestMapping("/edit")
+    public String doEdit(){
+        return "editktp";
+    }
+    
+}
